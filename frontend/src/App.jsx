5068f1035ecb6f8aaeb5cf838791f5a712f4f63b
santosh4568 <sk5580655@gmail.com>
2025-05-30 00:39:54 +0530

    Added rounting for Login and Register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import './App.css'; // We'll add some global styles here
+import './App.css';
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx"; // We'll add some global styles here
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 {/* Future routes can be added here */}
             </Routes>
         </Router>
